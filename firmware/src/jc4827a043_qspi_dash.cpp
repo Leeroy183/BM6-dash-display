e2532b95c9a8b23f977b8d618380b200542f3ef8
Lee <49012349+Leeroy183@users.noreply.github.com>
2026-08-07 09:56:56 +1000

    Keep native touch orientation

--- firmware/src/jc4827a043_qspi_dash.cpp
+++ firmware/src/jc4827a043_qspi_dash.cpp
@@ -106,8 +106,8 @@ class Gt911Touch {
 
         const int16_t rawX = static_cast<int16_t>(data[1] | (data[2] << 8));
         const int16_t rawY = static_cast<int16_t>(data[3] | (data[4] << 8));
-        point.x = SCREEN_WIDTH - 1 - constrain(rawX, 0, SCREEN_WIDTH - 1);
-        point.y = SCREEN_HEIGHT - 1 - constrain(rawY, 0, SCREEN_HEIGHT - 1);
+        point.x = constrain(rawX, 0, SCREEN_WIDTH - 1);
+        point.y = constrain(rawY, 0, SCREEN_HEIGHT - 1);
         contact = true;
         return true;
     }
